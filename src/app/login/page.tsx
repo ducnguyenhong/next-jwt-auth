42e4003220751b0bda1b0b930a69b4237592829c
IPA\duc.nguyenhong <duc.nguyenhong@ivnd.com.vn>
2023-11-10 16:59:34 +0700

    Update UI

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import Login from '@/client-page/login';
+import { NextPage } from 'next';
+
+const LoginPage: NextPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
---
+++ src/client-page/login/login.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { Box } from '@chakra-ui/react';
+
+const Login: React.FC = () => {
+  return <Box>a</Box>;
+};
+
+export default Login;
